da99644f02e3bd37b202011a05d0bfebcf91fb6f
reda aabbar <redaaabbar9@gmail.com>
2024-10-23 22:38:35 +0200

    first commit

---
+++ src/main/java/org/fsts/gestionbebliothequebackend/ProjecGestionBebliothequeBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.fsts.gestionbebliothequebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjecGestionBebliothequeBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjecGestionBebliothequeBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/fsts/gestionbebliothequebackend/ProjecGestionBebliothequeBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fsts.gestionbebliothequebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjecGestionBebliothequeBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
